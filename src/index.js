c802d0040f32b1000b5d7635bb1e1b0a599eec40
Noah8922 <juyeolgo@gmail.com>
2022-06-12 23:56:12 +0900

    using redux dev tool & redux-logger

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+); // 여러개의 미들웨어를 적용 할 수 있습니다.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
